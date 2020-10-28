f58db81f409d36f7f7f790452873fdfc7dbb65ef
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-10-28 23:44:01 +0300

    in views.py added class ShopView (link to shop.html)

--- vegefood/views.py
+++ vegefood/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+
 # Create your views here.
 class IndexView(View):
-
     def get(self, request):
         return render(request, 'vegefood/index.html')
+
+
+class ShopView(View):
+    def get(self, request):
+        return render(request, 'vegefood/shop.html')
