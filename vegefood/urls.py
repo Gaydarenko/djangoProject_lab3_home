fff164a720e2a6cfe7fa811d39ff027e12f77d28
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-10-29 20:45:56 +0300

    add name='home' in urls

--- vegefood/urls.py
+++ vegefood/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import IndexView, ShopView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='home'),
     path('shop/', ShopView.as_view(), name='shop'),
     path('shop/<int:page>', ShopView.as_view())
 ]
