4bce904f2112e0d07fb573de98b44e4294da7172
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-10-29 11:43:45 +0300

    add var

---
+++ vegefood/settings/info.py
@@ -0,0 +1,4 @@
+INFO = {
+    'phone': '8 812 987-65-32',
+    'email': 'test@vegefood.ru'
+}
--- vegefood/views.py
+++ vegefood/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from .settings.info import INFO
 
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'vegefood/index.html')
+        # contex = INFO.copy()
+        contex = {}
+        contex.update(INFO)
+        return render(request, 'vegefood/index.html', contex)
 
 
 class ShopView(View):
     def get(self, request):
-        return render(request, 'vegefood/shop.html')
+        contex = INFO.copy()
+        return render(request, 'vegefood/shop.html', contex)
